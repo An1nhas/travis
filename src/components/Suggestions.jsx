edeb67904e3c312b89fe953c1cbd5c3586695b5c
SaltyAga <aga.ania@gmail.com>
2019-03-13 22:21:30 +0100

    some changes in Suggestions

--- src/components/Suggestions.jsx
+++ src/components/Suggestions.jsx
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 
 
-const Suggestions = ({suggestions}) => {
-    var handleClickSugg = (e) => {
-        //console.log(document.getElementById("suggestion-field"));
-        document.getElementById("input-field").value =
-        document.getElementById("input-field").value + e.target.value;
-    };
+const Suggestions = ({suggestions, handleClickSugg}) => {
+
     const suggestionList = suggestions.length ? (
         suggestions.map((el, index) => {
             return (
