43c8ed368479ba08ed54ba9aa5b00dd78d29ca62
SaltyAga <aga.ania@gmail.com>
2019-03-14 09:43:08 +0100

    no eng KB, icons are added

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -10,23 +10,23 @@ const Keyboard = ({ keyboard, keyboardSet, shift, keyLang, handleClick,
     const keyLayout = keyboard.length ? (
         keyboard.map(key => {
 
-            let handleClickAll = keyLang === 'Tgr' && keyboardSet === 0 ? handleClickTgr : handleClick;
-            let keyCurrent = keyLang === 'Eng' ? (
-                keyboardSet === 0 && shift === false ? key.keyEng :
-                    keyboardSet === 0 && shift === true ? key.keyEngShift :
-                        keyboardSet === 1 ? key.keyNum : key.keyPunct) :
-                (keyboardSet === 0 && shift === false ? key.keyTgr :
-                    keyboardSet === 0 && shift === true ? key.keyTgrShift :
-                        keyboardSet === 1 ? key.keyNum : key.keyPunct);
+            let handleClickAll = keyboardSet === 0 ? handleClickTgr : handleClick;
+            let keyCurrent = (keyboardSet === 0 && shift === false) ? key.keyTgr : 
+                (keyboardSet === 0 && shift === true) ? key.keyTgrShift : 
+                keyboardSet === 1 ? key.keyNum : key.keyPunct;
+            let keyPrint = key.id ===  27 ? <MdBackspace /> : 
+                key.id === 31 ? <MdSpaceBar /> :
+                key.id === 32 ? <MdKeyboardReturn /> :
+                keyCurrent;
           
             return (
                 <span key={key.id}>
                     {shift === true && keyCurrent === "Shift" ? 
                         <button key={key.id}  className="btns" 
                         style={{color: "red"}} onClick={handleClickAll}
-                        value={keyCurrent} id={key.id}>{keyCurrent}</button> :
+                        value={keyCurrent} id={key.id}>{keyPrint}</button> :
                         <button key={key.id}  className="btns" onClick={handleClickAll}
-                        value={keyCurrent} id={key.id}>{keyCurrent}</button>
+                        value={keyCurrent} id={key.id}>{keyPrint}</button>
                     }
                     {show === true && shift === false ? (
                         keyCurrent === target ? (
